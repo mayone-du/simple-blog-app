dadc24519b3e22f1bcc07002b39068fe73a8e1b6
mayone-du <cocomayo1201@icloud.com>
2021-03-29 11:08:09 +0900

    update cors settings

--- simpleblogproject/settings.py
+++ simpleblogproject/settings.py
@@ -59,8 +59,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'simpleblogproject.urls'
 
-CORS_ORIGIN_WHIETLIST = [
-    "http://localhost:3000",
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:3000",
 ]
 
 SIMPLE_JWT = {
